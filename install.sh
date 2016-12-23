ddb7567b7e9d71433e265d684009ee3be11825cb
Martin Duarte <martinduartemore@gmail.com>
2016-12-23 20:35:05 -0200

    Use spaces instead of tabs

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@ set -o errexit
 set -o nounset
 
 # logging
-print_header()	{ printf "\e[1;37m$@\e[0m"		; }
-print_question(){ printf "\e[1;33m[?]\e[0m $@"	; }
-print_success()	{ printf "\e[1;32m[v]\e[0m $@"	; }
-print_error()	{ printf "\e[1;31m[x]\e[0m $@"	; }
-print_info()	{ printf "\e[1;36m[>]\e[0m $@"	; }
+print_header()  { printf "\e[1;37m$@\e[0m"      ; }
+print_question(){ printf "\e[1;33m[?]\e[0m $@"  ; }
+print_success() { printf "\e[1;32m[v]\e[0m $@"  ; }
+print_error()   { printf "\e[1;31m[x]\e[0m $@"  ; }
+print_info()    { printf "\e[1;36m[>]\e[0m $@"  ; }
 
 
 # utils
@@ -17,44 +17,44 @@ is_root() { [[ "${EUID}" = 0 ]]; }
 
 get_os()
 {
-	local os=""
-	
-	case "${OSTYPE}" in
-		linux*)		os="linux" ;;
-		darwin*)	os="macos" ;;
-		*)			os="unknown" ;;
-	esac
+    local os=""
+    
+    case "${OSTYPE}" in
+        linux*)     os="linux" ;;
+        darwin*)    os="macos" ;;
+        *)          os="unknown" ;;
+    esac
 
-	printf "%s" "${os}"
+    printf "%s" "${os}"
 }
 
 symlink_file()
 {
-	print_info "Attempting to symlink \"${1}\"\n"
-	local src="${dotfiles_dir}/${1}"
-	local dst="${HOME}/.${1}"
+    print_info "Attempting to symlink \"${1}\"\n"
+    local src="${dotfiles_dir}/${1}"
+    local dst="${HOME}/.${1}"
 
-	print_info "Deleting ${dst}\n"
-	rm -rf "${dst}"
+    print_info "Deleting ${dst}\n"
+    rm -rf "${dst}"
 
-	print_info "Copying ${src} to ${dst}\n"
-	ln -s "${src}" "${dst}"
+    print_info "Copying ${src} to ${dst}\n"
+    ln -s "${src}" "${dst}"
 }
 
 
 main()
 {
-	print_header "Gathering system information...\n"
-	declare -r os="$(get_os)"
-	declare -r dotfiles_dir="$(script_dir)" 
-	print_info "Operating system: ${os}\n"
-	print_info "Home directory: ${HOME}\n"
-	print_info "Dotfiles directory: ${dotfiles_dir}\n"
+    print_header "Gathering system information...\n"
+    declare -r os="$(get_os)"
+    declare -r dotfiles_dir="$(script_dir)" 
+    print_info "Operating system: ${os}\n"
+    print_info "Home directory: ${HOME}\n"
+    print_info "Dotfiles directory: ${dotfiles_dir}\n"
 
-	print_header "Symlinking files...\n"
-	symlink_file "vim"
+    print_header "Symlinking files...\n"
+    symlink_file "vim"
 
-	print_success "Done installing dotfiles!\n"
+    print_success "Done installing dotfiles!\n"
 }
 
 main "${@}"
