eeb31bc9d805eb84c517b8da20f9ad00ca2c1dd6
Martin Duarte More <martinduartemore@gmail.com>
2026-07-19 15:08:02 -0300

    feat(nvim): switch to Tokyo Night Storm with transparency

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -3,7 +3,7 @@ return {
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
     require("lualine").setup({
-      theme = "catppuccin-mocha"
+      theme = "tokyonight"
     })
   end
 }
